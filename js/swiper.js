0355a9b3e2ad39ef62824be523895c182134f834
kim geun young <119376320+geunyoungkim1998@users.noreply.github.com>
2023-04-11 18:00:27 +0900

    20230411

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: {
+        invert: true,
+    },
+    mousewheelControl:true,
+    speed:600,
+  });
