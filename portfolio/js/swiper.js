6fe09958a40790d6d75e1b45a1a852f707676725
kim geun young <119376320+geunyoungkim1998@users.noreply.github.com>
2023-04-24 09:56:04 +0900

    20230424

---
+++ portfolio/js/swiper.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    resistance:true,
+    resistanceRatio:0,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: {
+        invert: true,
+    },
+    mousewheelControl:true,
+    speed:600,
+  });
+
+  function go1(){
+    swiper.slideTo(0,600,false)
+  }
+  function go2(){
+    swiper.slideTo(1,600,false)
+  }
+  function go3(){
+    swiper.slideTo(2,800,false)
+  }
+  function go9(){
+    swiper.slideTo(8,1600,false)
+  }
